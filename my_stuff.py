1033ce220bafcb98f64484b353c514c85bbcd807
jesus <j3sus481@gmail.com>
2021-10-07 14:35:59 -0600

    first commit

---
+++ my_stuff.py
@@ -0,0 +1,9 @@
+from math import pi
+
+def sqr_foot(leng, wid):
+    area = leng * wid
+    return f"Area is {area}sq.ft"
+
+def circum(r):
+    circumference = 2 * (pi * r)
+    return f"Circumference is {circumference} feet"
